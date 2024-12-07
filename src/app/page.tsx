ce73b99fffeebe14e809d13a0a8cf00edda6b5b5
Jorge Antunes <ask@stoikerty.com>
2024-12-07 09:37:39 +0000

    add app logo and favicon

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,29 @@
 "use client";
+import Image from "next/image";
 import styled from "styled-components";
 
+const ImageBackground = styled.div`
+  display: flex;
+  padding: 16px;
+  background-color: #081014;
+`;
 const Text = styled.div`
   padding: 16px;
 `;
 
 export default function Home() {
-  return <Text>Antwerp plant</Text>;
+  return (
+    <>
+      <ImageBackground>
+        <Image
+          src="/app-logo.svg"
+          alt="Emitwise Logo"
+          width={20}
+          height={20}
+          priority
+        />
+      </ImageBackground>
+      <Text>Antwerp plant</Text>
+    </>
+  );
 }
